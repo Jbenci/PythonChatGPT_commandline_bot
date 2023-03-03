a47d4cd7de2976050817bd47ff84f5d2c8821ec4
Jbenci <Jasonbenci@gmail.com>
2023-03-03 08:10:37 +0200

    Add files via upload

---
+++ ChatGPTv2.py
@@ -0,0 +1,23 @@
+# import json
+import openai
+import os
+
+openai.api_key = "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"
+
+# system messages first, it helps set the behavior of the assistant.
+messages = [{"role": "system",
+             "content": "You are a helpful assistant."}]
+
+while True:
+    message = input("🫠: ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
+    reply = chat_completion.choices[0].message.content
+    print(f"🧁:{reply}")
+    messages.append({"role": "assistant", "content": reply})
